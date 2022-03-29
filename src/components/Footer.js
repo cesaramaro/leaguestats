842e108cd701f59acacbb21c35ad7f38a9f63271
Cesar <36521013+cesaramaro@users.noreply.github.com>
2022-03-29 00:32:52 -0400

    Use create-react-app, install Sass, create outline

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <footer>
+            <p className="">This page isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.</p>
+        </footer>
+    )
+}
