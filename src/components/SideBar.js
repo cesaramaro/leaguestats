10b52a3d5f77c80fbec0ba587c75d629a31415ce
Chris Toph <crchristopherrivera@gmail.com>
2022-04-01 02:47:18 -0400

    Replaced search icon .svg with SearcIco.js. Some coder refactoring in SearchBar.

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,7 +5,7 @@ import GithubIco from './icons/GithubIcon.js'
 export default function NavBar() {
     return (
 
-        <nav className="flex fixed p-6 font-Inter font-semibold text-2xl">
+        <nav className="fixed p-6 font-Inter font-semibold text-2xl">
             <div className="flex flex-col place-content-center w-20 rounded-bar-black ">
                 <p className="text-white text-center"> nt. </p>
                 <a className=' text-white pt-6' href="">
