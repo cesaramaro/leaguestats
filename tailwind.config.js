18a5bd1593d66c5dcf677fc977ee6355a639ec50
Chris Toph <crchristopherrivera@gmail.com>
2022-04-01 01:39:40 -0400

    added deleted theme changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '640': '40rem',
+      }
+    },
   },
   plugins: [],
 }
