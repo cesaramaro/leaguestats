96a5d46cf25a099d359c5e6fc8d79d9579b2eee2
Chris Toph <crchristopherrivera@gmail.com>
2022-04-01 19:20:28 -0400

    Created Clock,js for clock ico

---
+++ src/components/icons/Clock.js
@@ -0,0 +1,18 @@
+/**======================
+ **      CloclICo
+ * Returns svg of Clock logo.
+ * Enables easy colorization
+ * of svg and easy readability.
+ * fill='currentColor' is 
+ * what enables colorization
+ *========================**/
+
+export default function ClockIco() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2131 2130">
+            <g fill="fillCurrent">
+                <path d="M964 3a1068 1068 0 0 0-29 2121c49 7 213 7 264 0a1076 1076 0 0 0 926-927c7-50 7-214 0-263A1065 1065 0 0 0 1214 9c-51-8-199-12-250-6Zm203 272a794 794 0 0 1 343 1450A797 797 0 1 1 874 292c88-22 202-29 293-17Z" />
+                <path d="M1034 569c-29 17-28 9-28 273 0 231 0 238 8 252 5 8 15 18 23 23 14 8 20 8 308 8 286 0 294 0 308-8 17-10 30-32 30-52s-13-42-30-52c-14-8-23-9-269-10l-255-1-1-199c-2-182-2-200-9-212a59 59 0 0 0-53-30c-11 0-24 3-32 8Z" />
+            </g>
+        </svg>);
+}
